dea342fdcb81d6687ba532b2ab549f83a0c7197b
satu.heikkonen <satu.heikkonen@5CG05206R0.nordic.local>
2021-01-22 20:24:38 +0200

    Added user interaction and first methods

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,43 @@
-public class Program {
+import fileManipulation.*;
 
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        FileManipulation fileManipulation = new FileManipulation();
+
+        //Creates list of files in resources folder
+        File folder = new File("../src/resources"); 
+        File[] listOfFiles = folder.listFiles();     
+
+        //Keeps the interaction with user going until he/she will break it
+        while (true) {
+            System.out.println("What you want to do?");
+            System.out.println("1: List all the file names \n2: Get files by extension \n3: Get information about .txt file \nB: Break");
+            String option = scanner.nextLine(); 
+
+            //Breaks the loop if input is B or b
+            if (option.equals("B") || option.equals("b")){
+                scanner.close();
+                break;
+            }
+
+            //Check the input and call the right function
+            switch (option) {
+                case "1":
+                    fileManipulation.printAllNames(listOfFiles);
+                    break;
+                case "2":
+                    fileManipulation.printFilesByExtension(listOfFiles, scanner);
+                    break;
+                case "3":
+                    fileManipulation.textFileInformation(listOfFiles, scanner);
+                    break;
+            }
+        }
+
         
     }
 }
--- src/fileManipulation/FileManipulation.java
+++ src/fileManipulation/FileManipulation.java
@@ -1,5 +1,76 @@
 package fileManipulation;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Scanner;
+
 public class FileManipulation {
+
+    public void printAllNames(File[] listOfFiles) {
+        //Changes all files to strings and cut them for file names
+        System.out.println("-----------");
+        for (File file: listOfFiles){
+            String name = file.toString();
+            int lastChar = name.length();
+            String fileName = name.substring(17, lastChar);
+            System.out.println(fileName);
+        }
+    } 
+
+    public void printFilesByExtension(File[] listOfFiles, Scanner scanner) {
+        //Ask the extension from the user and check all files with the same extension
+        System.out.println("Type extension: ");
+        String ext = scanner.nextLine();
+        System.out.println("-----------");
+
+        for (File file: listOfFiles) {
+            String name = file.toString();
+            int lastChar = name.length();
+            String fileName = name.substring(17, lastChar);
+            if (file.toString().endsWith(ext)){
+                System.out.println(fileName);
+            }
+        }
+    }
+
+    public void textFileInformation(File[] listOfFiles, Scanner scanner) {
+        System.out.println("Type name of text file: ");
+        String name = scanner.nextLine();
+        System.out.println("-----------");
+        try {
+            File providedFile = getName(name, listOfFiles);
+            getSize(providedFile);
+
+        } catch (NullPointerException ex) {
+            System.out.println("There is no file called " + name);
+        }
+    }
+
+    public File getName(String name, File[] listOfFiles) {
+        File providedFile = null;
+        for (File file: listOfFiles) {
+            if (file.toString().endsWith(name)){
+                System.out.println(name);
+                providedFile = file;
+            }
+        }
+        return providedFile;
+    }
+
+    public void getSize(File name){
+        try(Reader fileReader = new FileReader(name)){
+            
+            int count = 0;
     
+            while((fileReader.read()) != -1) {
+                count = count + 1;
+            }
+            System.out.println(count);
+        }
+        catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
