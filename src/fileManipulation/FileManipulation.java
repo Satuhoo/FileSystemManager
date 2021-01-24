ba6ca741cc3943bc6eef57f291c1976102638444
satu.heikkonen <satu.heikkonen@5CG05206R0.nordic.local>
2021-01-24 11:20:02 +0200

    Added logging service

--- src/fileManipulation/FileManipulation.java
+++ src/fileManipulation/FileManipulation.java
@@ -5,9 +5,11 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class FileManipulation {
+    Logging logging = new Logging();
 
     public String fileToString(File file) {
         //Formulates file name and return it
@@ -18,11 +20,17 @@ public class FileManipulation {
     }
 
     public void printAllNames(File[] listOfFiles) {
+        LocalDateTime time = LocalDateTime.now();
+        long start = System.currentTimeMillis();
+        String log = "Printed all file names.";
         System.out.println("-----------");
         for (File file: listOfFiles){
             String fileName = fileToString(file);
             System.out.println(fileName);
         }
+        long end = System.currentTimeMillis();
+        logging.writeToLog(time, start, end, log);
+        
     } 
 
     public void printFilesByExtension(File[] listOfFiles, Scanner scanner) {
@@ -30,6 +38,9 @@ public class FileManipulation {
         System.out.println("Type extension: ");
         String ext = scanner.nextLine();
         System.out.println("-----------");
+        LocalDateTime time = LocalDateTime.now(); //Creates timestamp for logging
+        long start = System.currentTimeMillis();
+        String log = "Printed files by entered extension '" + ext + "'.";
 
         for (File file: listOfFiles) {
             if (file.toString().endsWith(ext)){
@@ -37,6 +48,8 @@ public class FileManipulation {
                 System.out.println(fileName);
             }
         }
+        long end = System.currentTimeMillis();
+        logging.writeToLog(time, start, end, log);
     }
 
     public void textFileInformation(File[] listOfFiles, Scanner scanner) {
@@ -55,6 +68,9 @@ public class FileManipulation {
     }
 
     public File getName(String name, File[] listOfFiles) {
+        LocalDateTime time = LocalDateTime.now();
+        long start = System.currentTimeMillis();
+        String log = "Searched file by entered name '" + name + "'.";
         File providedFile = null;
         for (File file: listOfFiles) {
             String fileName = fileToString(file);
@@ -64,13 +80,18 @@ public class FileManipulation {
                 providedFile = file;
             }
         }
+        long end = System.currentTimeMillis();
+        logging.writeToLog(time, start, end, log);
         return providedFile;
     }
 
     public void countCharacters(File name){
+        LocalDateTime time = LocalDateTime.now();
+        long start = System.currentTimeMillis();
+        int count = 0;
+
         try(Reader fileReader = new FileReader(name)){
             
-            int count = 0;
             //Reads all characters in the file and add them to amount
             while((fileReader.read()) != -1) {
                 count = count + 1;
@@ -80,12 +101,18 @@ public class FileManipulation {
         catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
+        String log = "The size of file was " + count + ".";
+        long end = System.currentTimeMillis();
+        logging.writeToLog(time, start, end, log);
     }
 
     public void countLines(File name) {
+        LocalDateTime time = LocalDateTime.now();
+        long start = System.currentTimeMillis();
+        int count = 0;
+
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(name))) {
 
-            int count = 0;
             //Reads all lines in the file and add them to amount
 
             while ((bufferedReader.readLine()) != null) {
@@ -96,16 +123,21 @@ public class FileManipulation {
         catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
+        String log = "There was " + count + " lines in the file '" + name + "'.";
+        long end = System.currentTimeMillis();
+        logging.writeToLog(time, start, end, log);
     }
 
     public void searchWord(File name, Scanner scanner) {
         System.out.println("\nType a word you want to search from the file: ");
         String word = scanner.nextLine();
+        LocalDateTime time = LocalDateTime.now();
+        long start = System.currentTimeMillis();
         String nextWord;
         Boolean wordExist = false; //default is false
+        int wordFound = 0;
 
         try(Scanner reader = new Scanner(name)){
-            int wordFound = 0;
     
             reader.useDelimiter(" "); //Separator is space between words
     
@@ -128,5 +160,8 @@ public class FileManipulation {
         catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
+        String log = "The term '" + word + "' was found " + wordFound + " times.";
+        long end = System.currentTimeMillis();
+        logging.writeToLog(time, start, end, log);
     }
 }
--- src/fileManipulation/Logging.java
+++ src/fileManipulation/Logging.java
@@ -1,5 +1,20 @@
 package fileManipulation;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 public class Logging {
-    
+
+    public void writeToLog (LocalDateTime time, Long start, Long end, String logText) {
+        try {
+            FileWriter myWriter = new FileWriter("../src/log/log.txt", true); //True is responsible for being able to add some content to the end of the file
+            myWriter.write("\n" + time + ": " + logText + " The function took " + (end - start) + "ms to execute.");
+            myWriter.write("\n");
+            myWriter.close();
+          } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
+    } 
 }
